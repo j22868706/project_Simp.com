e1af325dadf463ce82d7b9615d4b3cb7c7e36269
j22868706 <j22868706@gmail.com>
2023-11-07 23:37:49 -0600

    1107

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-    $(".card").on("click", function() {
-        $(".detail").addClass("active");
-    });
-    $(".close-detail").on("click", function() {
-        $(".detail").removeClass ("active")
-    });
+let card = document.querySelector(".card");
+let detail = document.querySelector(".detail");
+let closeDetailButton = document.querySelector(".close-detail");
+
+card.addEventListener("click", function() {
+    detail.classList.add("active");
 });
-    
-$(document).ready(function() {
-    $(".menu-bar").on("click", function() {
-        $(".sidebar").addClass("active")
-    });
+
+closeDetailButton.addEventListener("click", function() {
+    detail.classList.remove("active");
 });
-    
-$(".logo").on("click", function() {
-    $(".sidebar").removeClass("active");
+
+let menuBar = document.querySelector(".menu-bar");
+let sidebar = document.querySelector(".sidebar");
+let logo = document.querySelector(".logo");
+
+menuBar.addEventListener("click", function() {
+    sidebar.classList.add("active");
+});
+
+logo.addEventListener("click", function() {
+    sidebar.classList.remove("active");
 });
